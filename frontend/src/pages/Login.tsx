55c43ee972213519e98fbc61a4910a6d669ff323
vineetj12 <0123vineet@gmail.com>
2025-07-26 18:44:34 +0000

    add

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -20,7 +20,7 @@ export default function Login() {
 
     try {
       const response = await axios.post(
-        `http://localhost:3000/signin`,
+        `${BASE_URL}/signin`,
         { email, Password: password },
         {
           headers: {
